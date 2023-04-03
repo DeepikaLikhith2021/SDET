3b9722593b082b0befad1ff9dfa6e7f292a94cb1
likhith <likhithrg@gmail.com>
2023-04-03 13:36:37 +0530

    1st commit

---
+++ src/test/java/TEST/Practice1.java
@@ -0,0 +1,15 @@
+package TEST;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void test01() {
+		System.out.println("----execute1------");
+	}
+
+	public void test02() {
+		System.out.println("----execute2------");
+	}
+
+}
